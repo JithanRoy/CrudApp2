c7e3c91f182c96dbb8d31a0815263704ac61175f
Jithan Roy <jithanroyjony@gmail.com>
2023-02-02 13:01:53 +0000

    first commit

---
+++ src/components/Employee.js
@@ -0,0 +1,28 @@
+const Employee = [
+    {
+        id: 1,
+        Name: 'John',
+        age: 21,
+    },
+
+    {
+        id: 2,
+        Name: 'Jane',
+        age: 22,
+    },
+    {
+        id: 3,
+        Name: 'Mike',
+        age: 23,
+    },
+    {
+        id: 4,
+        Name: 'Mary',
+        age: 24,
+    },
+    {
+        id: 5,
+        Name: 'Nike',
+        age: 25,
+    }
+]
