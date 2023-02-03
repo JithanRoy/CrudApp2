93c6bc0e28c95a630347b830dad081f87da9d7a0
Jithan Roy <jithanroyjony@gmail.com>
2023-02-03 12:26:34 +0000

    last update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import React, { Component }  from 'react';
 import './App.css';
+import Home from './components/Home';
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-        
+        <BrowserRouter>
+              
+        </BrowserRouter>
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,48 @@
-import react from 'react';
+import React, { Fragment } from 'react';
+import {useForm} from "react-hook-form";
+import {Button, Table} from "react-bootstrap";
+import Employee from './Employee';
+import './Home.css';
 
-const 
+const Home = () => {
+    return (
+        <Fragment>
+            <div style={{margin:'10rem'}}>
+                <Table striped bordered hover size='sm'>
+                    <thead>
+                        <tr>
+                            <th>
+                                Name
+                            </th>
+                            <th>
+                                Age
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            Employee && Employee.length > 0
+                            ?
+                            Employee.map((item) => {
+                                return (
+                                    <tr>
+                                        <td>
+                                            {item.name}
+                                        </td>
+                                        <td>
+                                            {item.age}
+                                        </td>
+                                    </tr>
+                                )
+                            }) :
+                            "No data available"
+                        }
+
+                    </tbody>
+                </Table>
+            </div>
+        </Fragment>
+    );
+};
+
+export default Home;
